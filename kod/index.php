affc7d2704e22006b56b19cbc858c432d1dbba51
Marike Grinde <mg222cd@student.lnu.se>
2014-01-09 13:04:57 +0100

    uppdaterat kod

--- kod/index.php
+++ kod/index.php
@@ -12,7 +12,6 @@
 	</head>
 	
 	<body>
-		<p>test</p>
 		<?php
 			echo $xhtml;
 		?>
---
+++ kod/settings.php
@@ -0,0 +1,9 @@
+<?php
+ class DBSettings{
+ 	const DBHOST = "localhost";
+	const DBUSER = "root";
+	const DBUSER = "root";
+	const DATABASE = "Scrum";
+	const DBTABLEPREFIX = "members_";	
+ }
+?>
