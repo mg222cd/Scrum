8df6a40f7ad65c09ad8eb5f13d9913ae48d8f170
Marike Grinde <mg222cd@student.lnu.se>
2014-01-15 12:48:24 +0100

    mer kod

--- kod/Member.php
+++ kod/Member.php
@@ -11,8 +11,8 @@
 		
 		public function CreateMember($m_firstName, $m_lastName, $m_phone) {
 			$sql = "INSERT INTO $this->m_memberTableName(memberId, firstName, lastName, phone) VALUES(NULL, ?, ?, ?)";
-			$stmt = $this->m_dbConnection->Prepare($sql); //analyserar och förbereder
-			$stmt->bind_param("sss", $m_firstName, $m_lastName, $m_phone); //binder parametrar
+			$stmt = $this->m_dbConnection->Prepare($sql); 
+			$stmt->bind_param("sss", $m_firstName, $m_lastName, $m_phone); 
 			if ($stmt->execute()) {
 				$stmt->close();
 				return TRUE;
@@ -27,6 +27,28 @@
 		
 		public function EditMember($m_memberId, $m_firstName, $m_lastname, $m_phone) {
 			$sql = "UPDATE $this->m_memberTableName SET m_firstName=?, m_lastName=?, m_phone=? WHERE m_memberId=?";
+			$stmt = $this->m_dbConnection->Prepare($sql);
+			$stmt->bind_param("sssi", $m_firstName, $m_lastname, $m_phone, $m_memberId);
+			if ($stmt->execute()) {
+				$stmt->close();
+				return TRUE;
+			}
+			else {
+				return FALSE;
+			}
+		}
+		
+		public function DeleteMember($m_memberId) {
+			$sql = "DELETE FROM $this->m_memberTableName WHERE m_memberId=?";
+			$stmt = $this->m_dbConnection->Prepare($sql);
+			$stmt->bind_param("i", $m_memberId);
+			if ($stmt->execute()) {
+				$stmt->close();
+				return TRUE;
+			}
+			else {
+				return FALSE;
+			}
 		}
 		
 		
